712bf0067ee66270b93cd85d7fb29006a344ad87
claire froelich <claire.froelich@gmail.com>
2020-01-12 14:47:24 -0500

    setup with fake initial state cars

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
-// TODO: add and export your own actions
+export const FETCH_CARS = 'FETCH_CARS';
+
+export function fetchCars() {
+  // TODO
+}
---
+++ src/containers/cars_index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class CarsIndex extends Component {
+
+}
+
+export default CarsIndex;
